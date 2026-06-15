1cba1701b59a720a5107baf66d69db2d3ab2ccc0
Anzhu-W <anzhu.d@gmail.com>
2026-06-15 10:50:23 +1000

    add MERA (TKDE 2026) publication

--- publications-data.js
+++ publications-data.js
@@ -1,6 +1,12 @@
 /* Publications data source-of-truth (edit this file). */
 window.PUBLICATIONS = [
   {
+    "title": "MERA: A Green Edge Resource Control System with Privacy-Preservation via Mean-field Reinforcement Learning",
+    "authors": "Ziqi Wang, Xiaoyu Xia, Ibrahim Khalil, Feng Liu, Tianxu Lan, Xiaolong Xu, Xun Yi, Minhui Xue, Elisa Bertino",
+    "venue": "IEEE Transactions on Knowledge and Data Engineering (TKDE)",
+    "year": 2026
+  },
+  {
     "title": "Little By Little: Continual Learning via Incremental Mixture of Rank-1 Associative Memory Experts",
     "authors": "Haodong Lu, Chongyang Zhao, Minhui Xue, Lina Yao, Kristen Moore, Dong Gong",
     "venue": "International Conference on Machine Learning (ICML)",
